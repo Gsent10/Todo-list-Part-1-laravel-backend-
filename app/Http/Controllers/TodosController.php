a2ca6731c256292d169dd1340caadcbc5876e82f
Godsent Agundu <agundugodsent@gmail.com>
2023-03-06 09:37:22 +0100

    Initial commit

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::orderBy('created_at', 'DESC')->with('user')->get();
+
+        return response()->json([
+            'todos' => $todos,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => ['required', 'string', 'max:255'],
+            'userId' => ['required', 'integer', 'max:255']
+        ]);
+
+        return Todo::create([
+            'title' => $request->title,
+            'user_id' => $request->userId
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $todo = Todo::where('id', $id)->with('user')->get();
+
+        return response()->json([
+            'todo' => $todo,
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $todo = Todo::find($id);
+
+        $todo->update($request->all());
+
+        return response()->json([
+            'todo' => $todo,
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Todo::destroy($id);
+    }
+}
